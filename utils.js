1f8ac82fbacff431328ecac30e4f79f056e3ed2c
Suraj Sharma <suraj.sharma@epichost.in>
2024-04-14 14:35:52 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,11 +1,30 @@
+const fetch = require("node-fetch");
+
+const shortenUrl = async (url) => {
+  try {
+    const response = await fetch(`https://tinyurl.com/api-create.php?url=${url}`);
+    const shortenedUrl = await response.text();
+    return shortenedUrl;
+  } catch (error) {
+    console.error("Error shortening URL:", error);
+    return null; // Return null in case of error
+  }
+};
+
 const sendFile = async (item, ctx) => {
   if (item) {
     try {
-      await ctx.replyWithDocument(item);
+      const shortenedUrl = await shortenUrl(item);
+      if (shortenedUrl) {
+        await ctx.replyWithMarkdown(
+          `Click [here](${shortenedUrl}) to download the file.\n\n_Please note that the original URL may still be accessible._`
+        );
+      } else {
+        ctx.reply("Error occurred while shortening URL. Please try again later.");
+      }
     } catch (e) {
-      ctx.replyWithMarkdown(
-        `⚠️ ${e.message}\n\n👉 Try manually downloading from [here](${item})\n\n👉 *Maybe This File Is Too Large Or Cannot Accessible From Terabox*`,
-      );
+      console.error("Error sending file:", e);
+      ctx.reply("An error occurred while sending the file. Please try again later.");
     }
   }
 };
